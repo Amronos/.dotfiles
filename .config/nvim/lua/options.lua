8d07ab84ccaa13bac5ccbb5c51a2062b3601329b
Aarav Gupta <amronos275@gmail.com>
2024-12-16 12:27:48 +0530

    Add command to toggle format_on_save

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -30,3 +30,21 @@ opt.undodir = vim.env.HOME .. "/.undodir"
 opt.undofile = true
 opt.undolevels = 100
 opt.wrap = false
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,6 +1,11 @@
 return {
   "stevearc/conform.nvim",
+
   event = "BufWritePre",
+
+  -- This will provide type hinting with LuaLS
+  ---@module "conform"
+  ---@type conform.setupOpts
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
@@ -10,8 +15,12 @@ return {
       ["_"] = { "trim_whitespace" },
     },
 
-    format_on_save = {
-      lsp_fallback = true,
-    },
+    format_on_save = function(bufnr)
+      -- Disable with a global or buffer-local variable
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
+      return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
   },
 }
