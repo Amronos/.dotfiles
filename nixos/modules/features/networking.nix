ad85c0702b8d1493c9acb4ebcca74440581b5509
Aarav Gupta <aarav@spikonado.com>
2026-09-11 19:36:44 +0000

    chore(networking): Remove unused firewall ports (#14)

--- nixos/modules/features/networking.nix
+++ nixos/modules/features/networking.nix
@@ -6,11 +6,6 @@
       networking.networkmanager.enable = true;
       networking.firewall = {
         enable = true;
-        allowedTCPPorts = [
-          5173
-          7447
-        ];
-        allowedUDPPorts = [ 8888 ];
         extraCommands = ''
           iptables -A INPUT -p udp -d 224.0.0.0/4 -j ACCEPT
           iptables -A INPUT -p udp -s 224.0.0.0/4 -j ACCEPT
