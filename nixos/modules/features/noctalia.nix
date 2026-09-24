162903eebc2237f899ecf22c850a668c2c5ebe2c
Aarav Gupta <aarav@spikonado.com>
2026-09-24 08:43:39 +0530

    feat(desktop): Store Noctalia settings in dotfiles

--- nixos/modules/features/noctalia.nix
+++ nixos/modules/features/noctalia.nix
@@ -2,9 +2,17 @@
 {
   perSystem =
     { pkgs, ... }:
+    let
+      configDir = "/home/amronos/.dotfiles/noctalia";
+    in
     {
       packages.noctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
         inherit pkgs;
+        outOfStoreConfig = configDir;
+        settings = {
+          general.avatarImage = "${configDir}/avatar.svg";
+          wallpaper.directory = "${configDir}/wallpapers";
+        };
       };
     };
 }
